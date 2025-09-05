56620fc773950fa5d599f198d021f6c4d94b62d0
Eragbiscool <125035190+Eragbiscool@users.noreply.github.com>
2025-09-05 17:00:11 +0000

    testing 2ns again

--- test/tb_tasks.py
+++ test/tb_tasks.py
@@ -86,10 +86,10 @@ async def getcntr(dut,tqv):
 
 async def ext_clock(dut):
     for _ in range(100):
-        await Timer(3, units="ns")
         dut.ui_in[0].value = 1
         await Timer(2, units="ns")   # low for 1 ns
         dut.ui_in[0].value = 0
+        await Timer(2, units="ns")
         # await Timer(1, units="ns")   # high for 1 ns
 
 # ----------------------------------------------------------------------
