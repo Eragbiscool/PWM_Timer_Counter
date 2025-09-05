01e67a448eb378bd0dc3cc16db59c37a170b2313
Eragbiscool <125035190+Eragbiscool@users.noreply.github.com>
2025-09-05 17:14:57 +0000

    3ns and 4ns

--- test/tb_tasks.py
+++ test/tb_tasks.py
@@ -89,7 +89,7 @@ async def ext_clock(dut):
         dut.ui_in[0].value = 1
         await Timer(3, units="ns")   # low for 1 ns
         dut.ui_in[0].value = 0
-        await Timer(3, units="ns")
+        await Timer(4, units="ns")
         # await Timer(1, units="ns")   # high for 1 ns
 
 # ----------------------------------------------------------------------
