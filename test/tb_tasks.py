6ac70356e96339631838b07a4aeaa1b864260827
Eragbiscool <125035190+Eragbiscool@users.noreply.github.com>
2025-09-05 13:54:58 +0000

    Trying to ping ext clk

--- test/tb_tasks.py
+++ test/tb_tasks.py
@@ -126,9 +126,10 @@ async def test_eclk(dut,tqv):
     #     # await Timer(8, "ns")
     await Timer(800, "ns")
     for _ in range(100):
-        dut.ui_in[0].value = 0
-        await Timer(4, units="ns")
+        await Timer(1, "ns")
         dut.ui_in[0].value = 1
+        await Timer(4, units="ns")
+        dut.ui_in[0].value = 0
         await Timer(4, units="ns")
 
     cocotb.log.info("Wait Done")
