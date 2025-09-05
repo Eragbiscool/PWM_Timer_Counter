2d98cdc9f6fb3dce5d5a2f33576730bf82d564bc
Eragbiscool <125035190+Eragbiscool@users.noreply.github.com>
2025-09-05 09:52:41 +0000

    debugged

--- test/tb_tasks.py
+++ test/tb_tasks.py
@@ -126,7 +126,7 @@ async def test_eclk(dut,tqv):
         await Timer(4, units="ns")
         dut.ui_in[0].value = 1
         await Timer(4, units="ns")
-        cocotb.log.info(f"l2 = {l2}")
+        cocotb.log.info(f"l2 = {getcntr(dut,tqv)}")
 
     cocotb.log.info("Wait Done")
 
